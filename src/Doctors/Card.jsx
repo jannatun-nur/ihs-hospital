39b40e914a718c0dbdeda6f9d208eb40e25b7ba5
jannatun-nur <jannatunur03@gmail.com>
2024-05-25 09:09:04 +0600

    doctor 50%

--- src/Doctors/Card.jsx
+++ src/Doctors/Card.jsx
@@ -1,12 +1,17 @@
 
-
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { SlClock } from "react-icons/sl";
 const Card = ( { doctors }) => {
 
-    const {image , name } = doctors;
+    const {image , name, title, location , time} = doctors;
     return (
         <div>
-            <div className="bg-pink-500 ">
-            <img src={image} alt="" />
+            <div className="bg-white border border-green-600 rounded-lg shadow-2xl lg:w-9/12 lg:p-4 ">
+            <img className="rounded-full lg:ml-16 lg:w-5/12" src={image} alt="" />
+            <p className="text-green-700 lg:text-2xl font-bold">{name}</p>
+            <p className="text-green-700 lg:text-xl font-semibold py-2"> {title} </p>
+            <p className="flex gap-3 lg:text-xl text-green-700"><span className="lg:mt-1"><FaRegCalendarAlt /> </span>{ location}</p>
+            <p className="flex gap-3 lg:text-xl text-green-700 py-2"><span className="lg:mt-1"><SlClock /> </span>{time}</p>
             </div>
         </div>
     );
--- src/Doctors/Doctors.jsx
+++ src/Doctors/Doctors.jsx
@@ -17,7 +17,7 @@ const Doctors = () => {
     return (
         <div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4  lg:ml-24">
                 {
                     doctor.map(doctors=> <Card key={doctors} doctors={doctors}></Card>)
                 }
