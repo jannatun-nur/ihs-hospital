e1a26d2a64093da166acd7636b8a604f2aa36a92
jannatun-nur <jannatunur03@gmail.com>
2024-05-25 07:57:22 +0600

    create json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Banner from "./Banner/Banner"
 import Navbar from "./Navbar/Navbar"
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Banner/>
     </>
   )
 }
